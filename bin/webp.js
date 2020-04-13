fadd4e023c23ba196ce907dc57ab20037ec6a7d1
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-13 13:27:25 +0200

    Run Prettier on all files

--- bin/webp.js
+++ bin/webp.js
@@ -3,22 +3,22 @@ const imageminWebp = require('imagemin-webp')
 
 Promise.all([
   imagemin(['public/assets/images/*.{jpg,png}'], 'public/assets/images', {
-    use: [imageminWebp({ quality: 50 })]
+    use: [imageminWebp({ quality: 50 })],
   }),
   imagemin(
     ['public/assets/images/cards/*.{jpg,png}'],
     'public/assets/images/cards',
     {
-      use: [imageminWebp({ quality: 50 })]
+      use: [imageminWebp({ quality: 50 })],
     }
   ),
   imagemin(
     ['public/assets/images/puzzles/*.{jpg,png}'],
     'public/assets/images/puzzles',
     {
-      use: [imageminWebp({ quality: 50 })]
+      use: [imageminWebp({ quality: 50 })],
     }
-  )
+  ),
 ]).then(() => {
   console.log('Images optimized')
 })
